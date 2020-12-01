6addbe5a8b9ee37ceb15d248199cb8678f652a94
lj <luo_jun123123@163.com>
2020-12-01 17:16:18 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,14 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
+<script>
+export default {
+  name: "App",
+};
+</script>
 <style lang="less">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
